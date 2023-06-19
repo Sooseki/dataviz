f8a9676a5224f86de38b40430b34b22459c60113
david2he <david.dehe.pro@gmail.com>
2023-06-19 18:14:26 +0200

    init template bug  module export / import

--- backend/dist/index.js
+++ backend/dist/index.js
@@ -1,4 +1,4 @@
-import dotenv from 'dotenv'
+import dotenv from 'dotenv';
 import express from "express";
 import bodyParser from "body-parser";
 // import {connectDB} from "./config/database"
@@ -6,17 +6,14 @@ import test from './config/test';
 dotenv.config();
 const app = express();
 const port = process.env.PORT || 3000;
-console.log(test)
+console.log(test);
 // connectDB()
-
-app.use(bodyParser.urlencoded({ extended: false }))
-app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 app.use(bodyParser.json({ limit: '100mb' }));
-
 app.get('/', (req, res) => {
     res.send('Express + TypeScript Server');
 });
-
 app.listen(port, () => {
     console.log(`[server]: Server is running at http://localhost:${port}`);
 });
---
+++ backend/src/config/database.ts
@@ -0,0 +1,15 @@
+import mongoose from "mongoose";
+
+const connectDB = async () => {
+    console.log('5465165161651')
+    // try {
+    //     if(!process.env.MONGODB_URI) throw new Error("MONGODB_URI not provided");
+    //     await mongoose.connect(process.env.MONGODB_URI);
+    //     console.log("Connexion à la base de données réussie");
+    // } catch (error) {
+    //     console.log(error);
+    //     process.exit(1);
+    // }
+};
+
+export {connectDB}
