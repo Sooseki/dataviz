635109b8640e89c188e86879d6ee21fc94348fe7
Sooseki <manon.bonnet.30@gmail.com>
2023-06-20 10:42:44 +0200

    imports in node

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,13 +1,12 @@
 import dotenv from 'dotenv'
 import express from "express";
 import bodyParser from "body-parser";
-// import {connectDB} from "./config/database"
-import test from './config/test';
+import {connectDB} from "./config/database"
 dotenv.config();
 const app = express();
 const port = process.env.PORT || 3000;
-console.log(test)
-// connectDB()
+
+connectDB()
 
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
