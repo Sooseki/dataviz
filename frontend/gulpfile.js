6a52479a4850ced3314003a7f880c58e05cb6089
aparent <alexp90300@gmail.com>
2023-06-22 11:25:30 +0200

    feat: gukpwatch

--- frontend/gulpfile.js
+++ frontend/gulpfile.js
@@ -3,13 +3,18 @@ const rename = require('gulp-rename');
 const sass = require('gulp-sass')(require('sass'));
 
 // Define the Sass compilation task
-gulp.task('sass', function () {
+function compileSass() {
     return gulp
         .src('./src/styles/main.scss')
         .pipe(sass({ outputStyle: 'compressed' }).on('error', sass.logError))
         .pipe(rename('main.css'))
         .pipe(gulp.dest('./src/styles/'));
-});
+}
+
+// Define the watch task
+function watch() {
+    gulp.watch('./src/styles/**/*.scss', compileSass);
+}
 
 // Define the default task
-gulp.task('default', gulp.series('sass'));
+gulp.task('default', gulp.series(compileSass, watch));
