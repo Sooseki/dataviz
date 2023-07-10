a78cd6fd168d7a8b6e1651b6e890bbba8b443d5a
Sooseki <manon.bonnet.30@gmail.com>
2023-07-10 12:12:53 +0200

    init backend tests

--- backend/src/config/database.ts
+++ backend/src/config/database.ts
@@ -1,19 +1,11 @@
-import { connect, connection } from "mongoose";
+import { connect } from "mongoose";
 import { getEnvVariable } from '../utils/getEnvVariable';
-import User from "../models/User";
-import Client from "../models/Client";
-import Domain from "../models/Domain";
 
 export const connectDB = async (): Promise<void> => {
     try {
         const uri = getEnvVariable('MONGODB_URI');
         if(!uri) throw new Error("MONGODB_URI not provided");
         connect(uri);
-
-        connection.on('connected', async () => {
-            const clientDocuments = await connection.db.collection('clients').countDocuments();
-            if(getEnvVariable('NODE_ENV') === "dev" && clientDocuments === 0) loadTestDatas();
-        })
 
         // TODO : remove log
         console.log("Connexion à la base de données réussie");
@@ -23,22 +15,3 @@ export const connectDB = async (): Promise<void> => {
         process.exit(1);
     }
 };
-
-const loadTestDatas = async () => {
-    const user = await User.create({
-        name: "Manon Bonnet",
-        role: "admin",
-        email: "manon.bonnet.30@gmail.com",
-        password: "$2y$10$zZ3Gt9J23k9kZS8chH/VQuSP3Fjw3Api0oQhLS/gAeWYfSw5XSqhW",
-    });
-
-    const domain = await Domain.create({
-        url: "https://test-manon.com",
-    });
-
-    await Client.create({
-        name: "company name",
-        users: [user.id],
-        domains: [domain.id],
-    });
-}
---
+++ backend/src/utils/getEnvVariable.test.ts
@@ -0,0 +1,35 @@
+const mConfig = jest.fn();
+jest.mock("dotenv", () => ({
+    config: mConfig
+}));
+
+import path from "path";
+import { getEnvVariable } from "./getEnvVariable";
+
+describe("getEnvVariable", () => {
+    beforeEach(() => {
+        jest.resetAllMocks();
+    });
+
+    it("should get env variable", () => {
+        const envVariable = 'NODE_ENV';
+        const env = getEnvVariable(envVariable);
+
+        expect(mConfig).toHaveBeenCalledTimes(1);
+        expect(mConfig).toHaveBeenCalledWith({ path: path.resolve(__dirname, '../../.env') });
+        expect(env).toEqual("test");
+    })
+
+    it("should return undefined for non existing env variables", () => {
+        const envVariable = 'NODE_ENV_TEST';
+        const env = getEnvVariable(envVariable);
+
+        expect(mConfig).toHaveBeenCalledTimes(1);
+        expect(mConfig).toHaveBeenCalledWith({ path: path.resolve(__dirname, '../../.env') });
+        expect(env).toEqual(undefined);
+    })
+
+    it("should throw if no env variable provided", () => {
+        expect (() => getEnvVariable("")).toThrow("env not found");
+    })
+})
