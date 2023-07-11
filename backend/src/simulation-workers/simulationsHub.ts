5fc0d30517addcc6ddb2856c19c8b6d975a20e09
Juan Restrepo Torres <juan.m13@hotmail.fr>
2023-07-11 16:19:13 +0200

    metrics jest test working

--- backend/src/simulation-workers/simulationsHub.ts
+++ backend/src/simulation-workers/simulationsHub.ts
@@ -1,9 +1,10 @@
 //TO-DO : gather all workers data
 //TO-DO : single push to DB when all workers gathered
+import { IDataset } from "../types";
 import { timeToLoad } from "./workers/timeToLoad";
 //TO-DO : queue-system
 
-export const simulationhub = async (url: string) => {
+export const simulationhub = async (url: string): Promise<IDataset> => {
     const timeToLoadData = await timeToLoad(url);
     return { timeToLoad: timeToLoadData };
 };
---
+++ backend/src/types.ts
@@ -0,0 +1,3 @@
+export interface IDataset {
+    timeToLoad: number | undefined,
+} 
