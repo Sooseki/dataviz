5ebf48e5df361fc5694b200a02a968eb1a4857df
Juan Restrepo Torres <juan.m13@hotmail.fr>
2023-07-12 12:10:46 +0200

    fixed worker

--- backend/src/simulation-workers/workers/timeToLoad.ts
+++ backend/src/simulation-workers/workers/timeToLoad.ts
@@ -1,16 +1,11 @@
-/* eslint-disable @typescript-eslint/no-unused-vars */
-/* eslint-disable @typescript-eslint/no-var-requires */
-//import puppeteer from "puppeteer";
-const puppeteer = require("puppeteer");
+import puppeteer from "puppeteer";
 
-const timeToLoad = async () => {
-    const browser = await puppeteer.launch({ headless: "new" });
-
+export const timeToLoad = async (url: string) => {
+    const browser = await puppeteer.launch({ headless: "new" }); 
+    
     const page = await browser.newPage();
-    await page.goto("https://www.hetic.net/?gge_source=google&gge_medium=cpc&gge_term=hetic&gge_campaign=Search-MarquePure&gclid=Cj0KCQjwnrmlBhDHARIsADJ5b_kcYsjIFCG9t9mFJ1-3qSinhwgxrr984mnvDXI9r_XzL5FKmX9DZOcaAh75EALw_wcB");
-    const test = await page.metrics();
-    console.log(test);
+    await page.goto(url);
+    const {TaskDuration} = await page.metrics();
     await browser.close();
+    return TaskDuration;
 };
-
-timeToLoad();
