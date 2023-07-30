593a577f8bf3fdeeb5086646df520cecdc46a18e
Sooseki <bonnetmanon3006@gmail.com>
2023-07-30 10:21:48 +0200

    finalize nav without themes

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,18 +1,9 @@
 "use client";
-import Link from "next/link";
-import Navbar from "@/components/Navbar";
-import Burger from "@/components/Burger";
-
 const Home = () => {
 
     return (
         <div>
-            <Burger/>
-            <Navbar/>
-            <img className='logo' src="/perfguardian-text-and-logo.svg" alt='perfguardian-text-and-logo' />
-            <Link href="/login">
-                Login
-            </Link>
+            Dashboard
         </div>
     );
 };
---
+++ frontend/src/components/navbar/navconfig.ts
@@ -0,0 +1,17 @@
+export const navConfig = {
+  "Dashboard": {
+    name: "Dashboard",
+    href: "/",
+    icon: ""
+  },
+  "Item 2": {
+    name: "Item 2",
+    href: "/item",
+    icon: "icon"
+  },
+  "Item 3": {
+    name: "Item 3",
+    href: "/item-3",
+    icon: "icon"
+  },
+}
