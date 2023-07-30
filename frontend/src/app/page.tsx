218d540755fa827dfb3ec05d0d022057e8faf99b
Sooseki <bonnetmanon3006@gmail.com>
2023-07-30 11:13:44 +0200

    sample chart

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,9 +1,32 @@
 "use client";
+
+import Chart from "chart.js/auto";
+import { CategoryScale } from "chart.js";
+import TestChart from "@/components/charts/TestChart";
+
+Chart.register(CategoryScale);
+
 const Home = () => {
+    const data = {
+        labels: ['Red', 'Orange', 'Blue'],
+        datasets: [
+            {
+                label: 'Popularity of colours',
+                data: [55, 23, 96],
+                backgroundColor: [
+                    'rgba(255, 255, 255, 0.6)',
+                    'rgba(255, 255, 255, 0.6)',
+                    'rgba(255, 255, 255, 0.6)'
+                ],
+                borderWidth: 1,
+            }
+        ]
+    }
 
     return (
         <div>
             Dashboard
+            <TestChart data={data} />
         </div>
     );
 };
