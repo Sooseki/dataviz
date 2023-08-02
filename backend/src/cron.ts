8354cbe8ffb40fd6f7adfa2b01da13bc9bbd8dbb
Juan Restrepo Torres <juan.m13@hotmail.fr>
2023-08-02 12:30:59 +0200

    fixed everlasting cronjob :D

--- backend/src/cron.ts
+++ backend/src/cron.ts
@@ -2,7 +2,7 @@ import cron from "node-cron";
 import { runSimulationForAllDomains } from "./simulation-workers/simulationsHub";
 
 const createMetricsForAllDomains = () => {
-  cron.schedule("* */6 * * *", () => { // runs every 6 hours
+  cron.schedule("0 */6 * * *", () => { // runs every 6 hours
     runSimulationForAllDomains();
   })
 }
