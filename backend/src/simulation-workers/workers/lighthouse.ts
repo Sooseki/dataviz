5ad980c37d65dcc200d9f89277b796c722543f0f
Sooseki <bonnetmanon3006@gmail.com>
2023-08-04 11:25:14 +0200

    store metrics to ms numbers

--- backend/src/simulation-workers/workers/lighthouse.ts
+++ backend/src/simulation-workers/workers/lighthouse.ts
@@ -1,7 +1,7 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
 const lighthouse = require("lighthouse");
 const ReportGenerator = require("lighthouse/report/generator/report-generator");
-import puppeteer, { Browser } from "puppeteer";
+import puppeteer from "puppeteer";
 import { LighthouseMetrics } from "../../types";
 import { Options } from "chrome-launcher";
 const chromeLauncher = require("chrome-launcher");
@@ -10,32 +10,6 @@ import request from "request-promise-native";
 export const lighthouseFromPuppeteer = async (
     url: string
 ): Promise<LighthouseMetrics> => {
-    const results: LighthouseMetrics = {
-        firstContentfulPaint: "",
-        cumulativeLayoutShift: "",
-        totalBlockingTime: "",
-        timeToInteractive: ""
-    };
-
-    const { lighthouseBrowser, chrome, options } = await getLightHouseBrowser();
-
-    // Run Lighthouse
-    const { lhr } = await lighthouse(url, options);
-    const json = ReportGenerator.generateReport(lhr, "json");
-
-    const audits = JSON.parse(json).audits;
-    results.firstContentfulPaint = audits["first-contentful-paint"].displayValue;
-    results.cumulativeLayoutShift = audits["cumulative-layout-shift"].displayValue;
-    results.totalBlockingTime = audits["total-blocking-time"].displayValue;
-    results.timeToInteractive = audits["interactive"].displayValue;
-
-    lighthouseBrowser.disconnect();
-    await chrome.kill();
-
-    return results;
-};
-
-const getLightHouseBrowser = async (): Promise<{lighthouseBrowser: Browser, chrome: any, options: Options}> => {
     const options: Options = {
         logLevel: "info",
         chromeFlags: ["--disable-mobile-emulation"]
@@ -49,5 +23,30 @@ const getLightHouseBrowser = async (): Promise<{lighthouseBrowser: Browser, chro
     const resp = await request(`http://localhost:${options.port}/json/version`);
     const { webSocketDebuggerUrl } = JSON.parse(resp);
     const lighthouseBrowser = await puppeteer.connect({ browserWSEndpoint: webSocketDebuggerUrl });
-    return {lighthouseBrowser, chrome, options};
+
+    // Run Lighthouse
+    const { lhr } = await lighthouse(url, options);
+    const json = ReportGenerator.generateReport(lhr, "json");
+
+    const audits = JSON.parse(json).audits;
+
+    lighthouseBrowser.disconnect();
+    await chrome.kill();
+
+    return {
+        firstContentfulPaint: convertValueToMsNumber(audits["first-contentful-paint"].displayValue),
+        cumulativeLayoutShift: convertValueToMsNumber(audits["cumulative-layout-shift"].displayValue),
+        totalBlockingTime: convertValueToMsNumber(audits["total-blocking-time"].displayValue),
+        timeToInteractive: convertValueToMsNumber(audits["interactive"].displayValue)
+    };
 };
+
+const convertValueToMsNumber = (value: string): number => {
+    const formatedValue = value.replace(/,/g,""); // retrieve thousand separator ,
+    const [count, unit] = formatedValue.split(/\s/);
+
+    if (!unit || unit === "ms") return parseFloat(count);
+    if (unit === "s") return parseFloat(count) * 1000;
+
+    return 0
+}
