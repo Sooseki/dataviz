04aed90ccd7354ec87f90f5cd969fd62fc2f4c9a
David Dehe <david.dehe.pro@gmail.com>
2023-08-05 19:33:51 +0200

    fix/handleCall after rebase

--- frontend/src/api/handleCall.ts
+++ frontend/src/api/handleCall.ts
@@ -15,36 +15,21 @@ const defaultConfig = {
 
 
 export const handleGet = async <T>(url: string, config?: Record<string, unknown>): Promise<AxiosResponse<T> | undefined> => {
-    const res = await axios.get(url, config??defaultConfig)
-        .then((data) => data)
-        .catch((err) => console.error(err));
-export const handleGet = async <T>(url: string): Promise<AxiosResponse<T> | undefined> => {
-    const res = await axios.get(url, config).catch((err) => err.response);
-
+    const res = await axios.get(url, config??defaultConfig).catch((err) => err.response);
     if (!res) return undefined;
 
     return res;
 };
 
 export const handlePost = async <T>(url: string, data: Record<string, unknown>, config?: Record<string, unknown>): Promise<AxiosResponse<T> | undefined> => {
-    const res = await axios.post(url, data, config??defaultConfig)
-        .then((data) => data)
-        .catch((err) => console.error(err));
-export const handlePost = async <T>(url: string, data: {}): Promise<AxiosResponse<T> | undefined> => {
-    const res = await axios.post(url, data, config).catch((err) => err.response);
-    
+    const res = await axios.post(url, data, config??defaultConfig).catch((err) => err.response);
     if(!res) return undefined;
 
     return res;
 };
 
 export const handlePut = async <T>(url: string, data: Record<string, unknown>, config?: Record<string, unknown>): Promise<AxiosResponse<T> | undefined> => {
-    const res = await axios.put(url, data, config??defaultConfig)
-        .then((data) => data)
-        .catch((err) => console.error(err));
-export const handlePut = async <T>(url: string, data: {}): Promise<AxiosResponse<T> | undefined> => {
-    const res = await axios.put(url, data, config).catch((err) => err.response);
-    
+    const res = await axios.put(url, data, config??defaultConfig).catch((err) => err.response);
     if(!res) return undefined;
 
     return res;
