32f457c06079fee64e1d28c72257413efc6fc6fe
aparent <alexp90300@gmail.com>
2023-09-06 15:25:02 +0200

    fix: no data

--- backend/src/simulation-workers/simulationsHub.ts
+++ backend/src/simulation-workers/simulationsHub.ts
@@ -7,22 +7,26 @@ import puppeteer, { Browser } from "puppeteer";
 import Dataset from "../models/Dataset";
 import { IDomain } from "../types";
 
-const simulationhub = async (url: string, browser: Browser): Promise<IDataset | null> => {
+const simulationhub = async (
+    url: string,
+    browser: Browser
+): Promise<IDataset | null> => {
     try {
         const timeToLoadData = await timeToLoad(url, browser);
         const jsUseRateData = await jsUseRate(url, browser);
         const lighthouseFromPuppeteerData = await lighthouseFromPuppeteer(url);
-        return { 
+        return {
             timeToLoad: timeToLoadData,
             jsUseRate: jsUseRateData,
-            ...lighthouseFromPuppeteerData
+            ...lighthouseFromPuppeteerData,
         };
     } catch (e) {
-
         // TODO virer les console.error en prod
 
         if (e instanceof Error) {
-            console.error(`Erreur lors de la simulation pour ${url}: ${e.message}`);
+            console.error(
+                `Erreur lors de la simulation pour ${url}: ${e.message}`
+            );
         } else {
             console.error(`Erreur lors de la simulation pour ${url}: ${e}`);
         }
@@ -34,9 +38,9 @@ export const runSimulationForDomains = async (domains: IDomain[]) => {
     const browser = await puppeteer.launch({ headless: "new" });
 
     const metrics = [];
-    for(const domain of domains) {
+    for (const domain of domains) {
         const data = await simulationhub(domain.url, browser);
-        if (data) { 
+        if (data) {
             metrics.push({ domain, data });
         }
     }
--- frontend/src/components/charts/LastScan.tsx
+++ frontend/src/components/charts/LastScan.tsx
@@ -1,18 +1,19 @@
 import React, { useEffect } from "react";
 import { format, parseISO } from "date-fns";
-import { fr } from "date-fns/locale"; 
+import { fr } from "date-fns/locale";
 import { MetricsDataset } from "@/types";
 import { AxiosResponse } from "@/api/handleCall";
 
 interface PercentUsedListProps {
-    metricsData: AxiosResponse< {metrics: MetricsDataset[];}>;
+    metricsData: AxiosResponse<{ metrics: MetricsDataset[] }>;
 }
-
 
 const PercentUsedList: React.FC<PercentUsedListProps> = ({ metricsData }) => {
     useEffect(() => {
-        const progressInElements = document.querySelectorAll(".jsuserate_progressIn") as NodeListOf<HTMLElement>;
-            
+        const progressInElements = document.querySelectorAll(
+            ".jsuserate_progressIn"
+        ) as NodeListOf<HTMLElement>;
+
         progressInElements.forEach((element) => {
             const dataWidth = element.getAttribute("data-width");
             if (dataWidth) {
@@ -20,10 +21,18 @@ const PercentUsedList: React.FC<PercentUsedListProps> = ({ metricsData }) => {
             }
         });
     }, []);
-    if (!metricsData.data){
-        return null;
+
+    if (!metricsData.data || !metricsData.data.metrics.length) {
+        return <p>Pas encore de datas revenez plus tard</p>;
     }
-    const lastMetricsTable = metricsData.data.metrics[metricsData.data.metrics.length - 1];
+
+    const lastMetricsTable =
+        metricsData.data.metrics[metricsData.data.metrics.length - 1];
+
+    if (!lastMetricsTable) {
+        return <p>Pas encore de datas revenez plus tard</p>;
+    }
+
     const {
         jsUseRate,
         date,
@@ -34,30 +43,60 @@ const PercentUsedList: React.FC<PercentUsedListProps> = ({ metricsData }) => {
         timeToInteractive,
     } = lastMetricsTable;
 
-    const formattedDate = date ? format(parseISO(date), "d MMMM HH:mm", { locale: fr }) : "";
-
-    
+    const formattedDate = date
+        ? format(parseISO(date), "d MMMM HH:mm", { locale: fr })
+        : "";
 
     return (
         <div>
-            <h3 className="singledomain_lastScanTitle">Date du dernier scan: {formattedDate}</h3>
+            <h3 className="singledomain_lastScanTitle">
+                Date du dernier scan: {formattedDate}
+            </h3>
             <div className="singledomain_lastScanContainer">
-                <p className="singledomain_singleData"> <span>{timeToLoad}</span> timeToLoad </p>
-                <p className="singledomain_singleData"> <span>{firstContentfulPaint}</span> firstContentfulPaint </p>
-                <p className="singledomain_singleData"> <span>{cumulativeLayoutShift}</span> cumulativeLayoutShift  </p>
-                <p className="singledomain_singleData"> <span>{totalBlockingTime}</span> totalBlockingTime  </p>
-                <p className="singledomain_singleData"> <span>{timeToInteractive}</span> timeToInteractive  </p>
+                <p className="singledomain_singleData">
+                    {" "}
+                    <span>{timeToLoad}</span> timeToLoad{" "}
+                </p>
+                <p className="singledomain_singleData">
+                    {" "}
+                    <span>
+                        {firstContentfulPaint}
+                    </span> firstContentfulPaint{" "}
+                </p>
+                <p className="singledomain_singleData">
+                    {" "}
+                    <span>
+                        {cumulativeLayoutShift}
+                    </span> cumulativeLayoutShift{" "}
+                </p>
+                <p className="singledomain_singleData">
+                    {" "}
+                    <span>{totalBlockingTime}</span> totalBlockingTime{" "}
+                </p>
+                <p className="singledomain_singleData">
+                    {" "}
+                    <span>{timeToInteractive}</span> timeToInteractive{" "}
+                </p>
             </div>
-            <p className="jsuserate_alljsTitle">% d&apos; utilisation du Javascript</p>
+            <p className="jsuserate_alljsTitle">
+                % d&apos; utilisation du Javascript
+            </p>
             <ul className="jsuserate_alljs">
                 {jsUseRate ? (
                     jsUseRate.map((item, index) => (
                         <li className="jsuserate_singleFile" key={index}>
                             <div className="jsuserate_progressOut">
-                                <div data-width={item.percentUsed} className="jsuserate_progressIn"></div>
+                                <div
+                                    data-width={item.percentUsed}
+                                    className="jsuserate_progressIn"
+                                ></div>
                                 <p>{item.percentUsed}</p>
                             </div>
-                            <a href={item.url} target="_blank" rel="noopener noreferrer">
+                            <a
+                                href={item.url}
+                                target="_blank"
+                                rel="noopener noreferrer"
+                            >
                                 {item.url}
                             </a>
                         </li>
