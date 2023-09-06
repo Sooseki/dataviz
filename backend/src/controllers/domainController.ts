078ff43e2e48afb4276cef673a9fb7aa59b16125
Juan Restrepo Torres <juan.m13@hotmail.fr>
2023-09-06 12:04:50 +0200

    test

--- backend/src/controllers/domainController.ts
+++ backend/src/controllers/domainController.ts
@@ -6,18 +6,30 @@ import { handleControllerErrors } from "../utils/handleControllerErrors";
 import { IClientPopulated } from "@perfguardian/common/src/types";
 import { isValidUrl } from "../utils/domain";
 
-export const createDomain = async (req: Request, res: Response): Promise<Response> => {
+export const createDomain = async (
+    req: Request,
+    res: Response
+): Promise<Response> => {
     try {
-        const { url, clientId } = req.body as { url: string | undefined; clientId: string | undefined };
+        const { url, clientId } = req.body as {
+            url: string | undefined;
+            clientId: string | undefined;
+        };
 
         const urlIsValid = isValidUrl(url);
-        if (!urlIsValid) throw new Error("Domain could not be created. Wrong url param");
-        if (!clientId || typeof clientId !== "string") throw new Error("Domain could not be created for this client.");
+        if (!urlIsValid)
+            throw new Error("Domain could not be created. Wrong url param");
+        if (!clientId || typeof clientId !== "string")
+            throw new Error("Domain could not be created for this client.");
 
-        const client: IClientPopulated | null = await Client.findOne({ _id: clientId }).populate("domains");
-        if (!client) throw new Error("Domain could not be created. Client not found.");
+        const client: IClientPopulated | null = await Client.findOne({
+            _id: clientId,
+        }).populate("domains");
+        if (!client)
+            throw new Error("Domain could not be created. Client not found.");
 
-        if (clientDomainExists(client, url)) throw new Error("Domain already exists");
+        if (clientDomainExists(client, url))
+            throw new Error("Domain already exists");
 
         const newDomain = await Domain.create({ url });
 
@@ -28,17 +40,21 @@ export const createDomain = async (req: Request, res: Response): Promise<Respons
             }
         );
 
-        return res.status(200).json({data:{ domain: newDomain }});
+        return res.status(200).json({ data: { domain: newDomain } });
     } catch (err) {
         return handleControllerErrors(err, res, "domain could not be created");
     }
 };
 
-export const getDomains = async (req: Request, res: Response): Promise<Response> => {
+export const getDomains = async (
+    req: Request,
+    res: Response
+): Promise<Response> => {
     try {
         // TODO : get clientId from token
         const { clientId } = req.query as { clientId: string | undefined };
-        if (!clientId || typeof clientId !== "string") throw new Error("Cannot get domains for this client.");
+        if (!clientId || typeof clientId !== "string")
+            throw new Error("Cannot get domains for this client.");
 
         const client = await Client.findOne({ _id: clientId });
         if (!client) throw new Error("Client not found.");
@@ -47,7 +63,7 @@ export const getDomains = async (req: Request, res: Response): Promise<Response>
             _id: { $in: client.domains },
         });
 
-        return res.status(200).json({data:{ domains }});
+        return res.status(200).json({ data: { domains } });
     } catch (err) {
         return handleControllerErrors(err, res, "could not get domains");
     }
