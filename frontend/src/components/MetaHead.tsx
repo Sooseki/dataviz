edb41a7797df64921d1eb1eb752054a9df2229cc
aparent <alexp90300@gmail.com>
2023-09-08 11:01:29 +0200

    feat: ajout favicon

---
+++ frontend/src/components/MetaHead.tsx
@@ -0,0 +1,10 @@
+import Head from "next/head";
+
+const MetaHead: React.FC = () => (
+    <Head>
+        {/* Pour un fichier .ico */}
+        <link rel="shortcut icon" href="/favicon.ico" />
+    </Head>
+);
+
+export default MetaHead;
