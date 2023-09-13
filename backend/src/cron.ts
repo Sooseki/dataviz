66ac2234dd6a93ffdf000d14ab8dd513e8c2ddf6
Sooseki <manon.bonnet.30@gmail.com>
2023-09-13 11:03:19 +0200

    undo cron update

--- backend/src/cron.ts
+++ backend/src/cron.ts
@@ -2,7 +2,7 @@ import cron from "node-cron";
 import { runSimulationForAllDomains } from "./simulation-workers/simulationsHub";
 
 const createMetricsForAllDomains = () => {
-    cron.schedule("*/5 * * * *", () => {
+    cron.schedule("0 */6 * * *", () => {
         // runs every 6 hours
         runSimulationForAllDomains();
     });
