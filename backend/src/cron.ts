049f39f41c44f4401004aae8b8a239ec060601af
Sooseki <manon.bonnet.30@gmail.com>
2023-09-13 11:01:41 +0200

    optimize use of lighthouse

--- backend/src/cron.ts
+++ backend/src/cron.ts
@@ -2,7 +2,7 @@ import cron from "node-cron";
 import { runSimulationForAllDomains } from "./simulation-workers/simulationsHub";
 
 const createMetricsForAllDomains = () => {
-    cron.schedule("0 */6 * * *", () => {
+    cron.schedule("*/5 * * * *", () => {
         // runs every 6 hours
         runSimulationForAllDomains();
     });
