326a254da3ff5e86175acf6084bf8432e5a7ab71
Maroua <mezned.marwaa@gmail.com>
2023-10-05 11:04:20 +0200

    cron qui supprime les datas qui ont plus que 3 mois

--- backend/src/cron.ts
+++ backend/src/cron.ts
@@ -1,5 +1,6 @@
 import cron from "node-cron";
 import { runSimulationForAllDomains } from "./simulation-workers/simulationsHub";
+import { deleteData } from "../scripts/deleteOldData";
 
 const createMetricsForAllDomains = () => {
     cron.schedule("0 */6 * * *", () => {
@@ -7,5 +8,12 @@ const createMetricsForAllDomains = () => {
         runSimulationForAllDomains();
     });
 };
+const deleteOldData = () => {
+    cron.schedule("0 0 1 */3 *", () => {
+        // runs every 6 hours
+        deleteData();
+    });
+};
 
 export { createMetricsForAllDomains };
+export { deleteOldData };
