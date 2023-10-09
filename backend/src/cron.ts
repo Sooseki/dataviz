9d3ce0cbca314b071b42b8a80f8c66b1d8bca026
Maroua <mezned.marwaa@gmail.com>
2023-10-09 10:53:40 +0200

    web-vitals

--- backend/src/cron.ts
+++ backend/src/cron.ts
@@ -5,12 +5,26 @@ import { deleteData } from "../scripts/deleteOldData";
 const createMetricsForAllDomains = () => {
     cron.schedule("0 */6 * * *", () => {
         // runs every 6 hours
-        runSimulationForAllDomains();
+        const maxCronExecute = 9000;
+        const cronTimeout = setTimeout(() => {
+            console.log('Dépassement de la limite du temps');
+
+        }, maxCronExecute);
+
+        runSimulationForAllDomains()
+        .then(() =>{
+            clearTimeout(cronTimeout);
+            console.log("Le cron est exécuté avec succées");
+        })
+        .catch((error) =>{
+            clearTimeout(cronTimeout);
+            console.error("Erreur lors de l'éxécution de la tçache cron :" ,error);
+        })
     });
 };
 const deleteOldData = () => {
     cron.schedule("0 0 1 */3 *", () => {
-        // runs every 6 hours
+        // runs every 3 months
         deleteData();
     });
 };
--- frontend/src/components/MetaHead.tsx
+++ frontend/src/components/MetaHead.tsx
@@ -2,6 +2,29 @@ import Head from "next/head";
 
 const MetaHead: React.FC = () => (
     <Head>
+       
+    <script>
+        (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+        })(window,document,'script','dataLayer','GTM-NQ34QDHL');
+    </script>
+    
+    <div id="diagnostics-container"></div>
+    <script>
+        // Utilisez fetch pour récupérer le contenu de diagnostics.html
+        fetch('../../../scripts/diagnostics.html')
+            .then(response => response.text())
+            .then(html => {
+                // Ajoutez le contenu de diagnostics.html à la div avec l'ID "diagnostics-container"
+                document.getElementById('diagnostics-container').innerHTML = html;
+            })
+            .catch(error => {
+                console.error('Erreur lors du chargement de diagnostics.html :', error);
+            });
+    </script>
+    
         <link rel="shortcut icon" href="/favicon.ico" />
     </Head>
 );
