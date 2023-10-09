b68430be208f184296c19eea11d592e5526eb67b
Maroua <mezned.marwaa@gmail.com>
2023-10-09 11:29:12 +0200

    web-vitals

--- frontend/src/components/MetaHead.tsx
+++ frontend/src/components/MetaHead.tsx
@@ -3,13 +3,15 @@ import Head from "next/head";
 const MetaHead: React.FC = () => (
     <Head>
        
-    <script>
-        (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-        })(window,document,'script','dataLayer','GTM-NQ34QDHL');
-    </script>
+       <!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-WR2M7JXLC3"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-WR2M7JXLC3');
+</script>
     
     <div id="diagnostics-container"></div>
     <script>
