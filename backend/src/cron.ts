370119dc094e671b654a7d6f074ff7f8ddf186fa
Sooseki <manon.bonnet.30@gmail.com>
2023-10-15 15:32:40 +0200

    launch chrome only once a day

--- backend/src/cron.ts
+++ backend/src/cron.ts
@@ -2,8 +2,8 @@ import cron from "node-cron";
 import { runSimulationForAllDomains } from "./simulation-workers/simulationsHub";
 
 const createMetricsForAllDomains = () => {
-    cron.schedule("0 */6 * * *", () => {
-        // runs every 6 hours
+    cron.schedule("0 2 * * *", () => {
+        // runs every day at 2am
         runSimulationForAllDomains();
     });
 };
